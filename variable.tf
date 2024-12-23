58465f3dd77d7ca828e475984e855d32ab3e73ed
kiran8884146600 <143705462+kiran8884146600@users.noreply.github.com>
2024-12-23 18:17:33 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,20 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-west-2"
+}
+variable "aws_access_key" {
+  description = "AWS_key"
+  type        = string
+  default     = "12344565666777"
+}
+variable "aws_secret_key" {
+  description = "AWS_key"
+  type        = string
+  default     = "12345567899"
+}
+variable "ami" {
+  description = "AWS_ami"
+  type        = string
+  default     = "asddfgghg1232343"
+}
