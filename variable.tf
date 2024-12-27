2752d9b5424d6dd8b851571c146831881b384809
kiran8884146600 <143705462+kiran8884146600@users.noreply.github.com>
2024-12-27 12:46:22 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,20 +1,20 @@
 variable "aws_region" {
   description = "AWS region"
   type        = string
-  default     = "us-west-2"
+  default     = "us-east-1"
 }
 variable "aws_access_key" {
   description = "AWS_key"
   type        = string
-  default     = "12344565666777"
+  default     = "secrete.AWS_ACCESS_KEY_ID"
 }
 variable "aws_secret_key" {
   description = "AWS_key"
   type        = string
-  default     = "12345567899"
+  default     = "secrete.AWS_SECRET_ACCESS_KEY"
 }
 variable "ami" {
   description = "AWS_ami"
   type        = string
-  default     = "asddfgghg1232343"
+  default     = "ami-01816d07b1128cd2d"
 }
