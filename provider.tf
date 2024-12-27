77f083030fc5289ac07d5246c97565b5759a8127
kiran8884146600 <143705462+kiran8884146600@users.noreply.github.com>
2024-12-27 13:14:52 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,11 +9,10 @@ terraform {
 
 
 provider "aws" {
- access_key = var.aws_access_key
- secret_key = var.aws_secret_key
- region     = var.aws_region
+  region     = "us-east-1"
+  access_key = "Secrete.AWS_ACCESS_KEY_ID"
+  secret_key = "Secrete.AWS_SECRET_ACCESS_KEY"
 }
-
 resource "aws_instance" "ins1" {
   ami           = var.ami
   instance_type = "t3.micro"
